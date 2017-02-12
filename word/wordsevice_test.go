4fe3a141cd4ca417dd56801c9cbdce4574ecac99
Samuel Bisciglia <samuelbisciglia@Samuels-MacBook-Pro.local>
2017-02-11 18:58:23 -0600

    made tests less dumb

--- word/wordsevice_test.go
+++ word/wordsevice_test.go
@@ -2,6 +2,7 @@ package word
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/sbiscigl/anagramaasaservice/dictionary"
@@ -9,7 +10,8 @@ import (
 
 func TestPermuations(t *testing.T) {
 	data := NewData()
-	dcData := dictionary.NewDataFromFile("/Users/samuelbisciglia/go/src/github.com/sbiscigl/anagramaasaservice/resources/dictionary.txt")
+	path := os.Getenv("GOPATH")
+	dcData := dictionary.NewDataFromFile(path + "/src/github.com/sbiscigl/anagramaasaservice/resources/dictionary.txt")
 	dcService := dictionary.NewService(dcData)
 	b := NewService(data, dcService)
 	a := b.GetAnagrams("read")
